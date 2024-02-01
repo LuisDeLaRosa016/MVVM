2e4aa7177ee8513bb36d17de3309ff672e30ab6c
LuisDeLaRosa016 <ldelarosa49@gmail.com>
2024-02-01 11:36:03 -0600

    #feature/Cine: AppCine project initialization.

---
+++ AppCine/AppCine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppCine
+//
+//  Created by Luis De La Rosa on 01/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
